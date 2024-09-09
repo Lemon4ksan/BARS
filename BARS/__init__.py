7491fa6c4dbf5fcf0fef52120beedc4c978852fc
Lemon4ksan <senya20151718@gmail.com>
2024-09-09 16:40:20 +0300

    Обновлён __init__.py

--- BARS/__init__.py
+++ BARS/__init__.py
@@ -1,13 +1,54 @@
 from ._base import ClientObject
 
 from ._client import BClient
+from ._client_async import BClientAsync
 
 from ._diary import DiaryDay
 from ._diary import DiaryLesson
 
+from ._homework import HomeworkDay
+from ._homework import HomeworkLesson
+
+from ._account import AccountInfo
+from ._account import PupilInfo
+from ._account import UnlockedDiscilpine
+
+from ._schedule import ScheduleLesson
+from ._schedule import DaySchedule
+from ._schedule import MonthSchedule
+
+from ._school import SchoolInfo
+from ._school import ClassInfo
+from ._school import Pupil
+from ._school import Employee
+
+from ._marks import SummaryMarks
+from ._marks import Mark
+from ._marks import TotalMarks
+from ._marks import TotalDisciplineMarks
+from ._marks import SummaryDisciplineMarks
+
 __all__ = [
-    'BClient',
     'ClientObject',
+    'BClient',
+    'BClientAsync',
     'DiaryDay',
-    'DiaryLesson'
+    'DiaryLesson',
+    'HomeworkDay',
+    'HomeworkLesson',
+    'AccountInfo',
+    'PupilInfo',
+    'UnlockedDiscilpine',
+    'ScheduleLesson',
+    'DaySchedule',
+    'MonthSchedule',
+    'SchoolInfo',
+    'ClassInfo',
+    'Pupil',
+    'Employee',
+    'SummaryMarks',
+    'Mark',
+    'TotalMarks',
+    'TotalDisciplineMarks',
+    'SummaryDisciplineMarks'
 ]
